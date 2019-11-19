031af7e60665a160941372f30bdf224dbb52bf8d
abrin <abrin@DESKTOP-EF02NM0.dhcp.fh-bielefeld.de>
2019-11-19 16:33:31 +0100

    1

---
+++ Bonus/Blatt_4/MVC_Observer/IModel.java
@@ -0,0 +1,27 @@
+package MVC_Observer;
+
+public interface IModel {
+
+	/**
+	 * Anmelden einer View.
+	 * 
+	 * @param view
+	 */
+	public void anmelden(IView view);
+
+	/**
+	 * Abmelden einer View.
+	 * 
+	 * @param view
+	 */
+	public void abmelden(IView view);
+	
+	
+	/**
+	 * Abfragen des Semesters.
+	 * 
+	 * @return
+	 */
+	public String getSemester();
+	
+}
---
+++ Bonus/Blatt_4/MVC_Observer/IView.java
@@ -0,0 +1,11 @@
+package MVC_Observer;
+public interface IView {
+
+	/**
+	 * Die aktualisieren Methode aktualisiert die View.
+	 * 
+	 */
+	public void aktualisieren(IModel model);
+	
+	
+}
